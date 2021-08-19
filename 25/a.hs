9daf1d9d48c39277a91d2cd8f1df4e508aeae2c5
Parranoh <minerva.schlau@hotmail.com>
2021-08-19 20:45:12 +0200

    day 25a: encode n in type of group

--- 25/a.hs
+++ 25/a.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import qualified Data.IntMap.Strict as M
 import Data.Bifunctor (bimap,first)
 
@@ -19,55 +21,70 @@ class Monoid m => Group m where
                 | n == 1 = x `mappend` c
                 | otherwise = g (x `mappend` x) (n `quot` 2) (x `mappend` c)
 
-newtype G = G { unG :: Int }
-n :: Int
-n = 20201227
+data G a = G { unG :: Int }
 
-instance Semigroup G where
-    G a <> G b = G $ a * b `mod` n
+data Zero
+data B0 a
+data B1 a
+numPred0 :: B0 a -> a
+numPred0 = undefined
+numPred1 :: B1 a -> a
+numPred1 = undefined
+class Number a where
+    numValue :: a -> Int
+instance Number Zero where
+    numValue = const 0
+instance (Number a) => Number (B0 a) where
+    numValue x = 2 * numValue (numPred0 x)
+instance (Number a) => Number (B1 a) where
+    numValue x = 2 * numValue (numPred1 x) + 1
+type N = B1 (B1 (B0 (B1 (B0 (B0 (B0 (B0 (B1 (B1 (B1 (B1 (B1 (B1 (B0 (B0 (B0 (B0 (B1 (B0 (B1 (B1 (B0 (B0 (B1 Zero)))))))))))))))))))))))) -- 20201227
 
-instance Monoid G where
+instance (Number a) => Semigroup (G a) where
+    G x <> G y = G $ x * y `mod` numValue (undefined :: a)
+
+instance (Number a) => Monoid (G a) where
     mempty = G 1
 
-instance Group G where
-    invert (G a) = G $ euklid a n
+instance (Number a) => Group (G a) where
+    invert (G x) = G $ euklid x (numValue (undefined :: a))
 
 euklid :: Int -> Int -> Int
-euklid a n = go 0 1 n a
+euklid x n = go 0 1 n x
     where
         go t _ r 0
-            | r > 1     = error "a is not invertible"
+            | r > 1     = error "not invertible"
             | t < 0     = t + n
             | otherwise = t
         go t newt r newr =
             let quotient = r `div` newr
             in go newt (t - quotient * newt) newr (r - quotient * newr)
 
-babyStepGiantStep :: G -> G -> Int
-babyStepGiantStep a b =
-    let m = ceiling . sqrt . fromIntegral $ n
+babyStepGiantStep :: forall a. (Number a) => G a -> G a -> Int
+babyStepGiantStep b x =
+    let m = ceiling . sqrt . fromIntegral $ numValue (undefined :: a)
         table =
             M.fromList .
             map (first unG) .
             take m .
-            iterate (bimap (<> a) succ) $
+            iterate (bimap (<> b) succ) $
             (mempty,0)
-        a_m = pow a (-m)
-        go i c = if i == m then error "loop ended" else
+        b_m = pow b (-m)
+        go i c = if i == m then error "discrete log does not exist" else
             case M.lookup (unG c) table of
-                Nothing -> i `seq` go (succ i) (c <> a_m)
+                Nothing -> i `seq` go (succ i) (c <> b_m)
                 Just j  -> i * m + j
-    in go 0 b
+    in go 0 x
 
 main = interact $ output . solve . input
 
-input :: String -> (G,G)
+input :: String -> (G N,G N)
 input s = let [a,b] = lines s in (G $ read a,G $ read b)
 
-solve :: (G,G) -> G
-solve (a,b) =
-    let loopSizeA = babyStepGiantStep (G 7) a
-    in pow b loopSizeA
+solve :: (Number a) => (G a,G a) -> G a
+solve (x,y) =
+    let loopSizeX = babyStepGiantStep (G 7) x
+    in pow y loopSizeX
 
-output :: G -> String
+output :: G a -> String
 output = show . unG
